3865fc4e3f5373fa33d4246fa24c01ec9cf3e926
frichard97 <foldesi.richard97@gmail.com>
2019-03-02 13:27:53 +0100

    Set UP MAIN MENU

--- core/src/hu/legjava/game/Main.java
+++ core/src/hu/legjava/game/Main.java
@@ -2,14 +2,33 @@ package hu.legjava.game;
 
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import hu.legjava.game.Menus.StartingScreen;
 
 public class Main extends Game {
 	static public AssetManager manager;
+	public enum STATES{
+		SINGLEPLAYER,
+		SERVER,
+		CLIENT
+	}
+	static public STATES state;
+
+	public static Skin getSkin() {
+		return skin;
+	}
+
+	public static void setSkin(Skin skin) {
+		Main.skin = skin;
+	}
+
+	static private Skin skin;
 	@Override
 	public void create () {
 		manager = new AssetManager();
+		skin = new Skin(Gdx.files.internal("uiskin.json"));
 		setScreen(new StartingScreen(this));
 	}
 
--- core/src/hu/legjava/game/Menus/MainMenu.java
+++ core/src/hu/legjava/game/Menus/MainMenu.java
@@ -1,26 +1,44 @@
 package hu.legjava.game.Menus;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import hu.legjava.game.Main;
 
 public class MainMenu implements Screen {
-    Main main;
-    Button SinglePlayer;
-    Button MultiPlayer;
-    Button Exit;
+    private Main main;
+    private TextButton singlePlayer;
+    private TextButton multiPlayer;
+    private TextButton exit;
+    private Stage stage;
+    private Table table;
     public MainMenu(Main main)
     {
         this.main = main;
     }
     @Override
     public void show() {
+            stage = new Stage();
+            table = new Table();
+            table.setFillParent(true);
+            singlePlayer = new TextButton("SinglePlayer",main.getSkin());
+            multiPlayer = new TextButton("MultiPlayer",main.getSkin());
+            exit = new TextButton("Exit",main.getSkin());
+            table.add(singlePlayer).size(200,90).spaceBottom(5f).row();
+            table.add(multiPlayer).size(200,90).spaceBottom(5f).row();
+            table.add(exit).size(200,90).spaceBottom(5f).row();
+            stage.addActor(table);
+            Gdx.input.setInputProcessor(stage);
 
     }
 
     @Override
     public void render(float delta) {
-
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
