781e044434e02d89a82d13f5fff15e72e3912203
frichard97 <foldesi.richard97@gmail.com>
2019-03-02 18:58:54 +0100

    Map files added

---
+++ core/assets/base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="base" tilewidth="100" tileheight="100" tilecount="1" columns="1">
+ <image source="Grass.png" width="100" height="100"/>
+</tileset>
